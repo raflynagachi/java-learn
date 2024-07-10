88852fec725d61a62f6763871386953de8452dae
raflynagachi <rafly.rigannagachi@gmail.com>
2024-07-10 17:16:13 +0700

    unit test: before after each

--- java-unit-test/src/test/java/raflynagachi/test/CalculatorTest.java
+++ java-unit-test/src/test/java/raflynagachi/test/CalculatorTest.java
@@ -2,11 +2,29 @@ package raflynagachi.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
+
+    @BeforeAll // should be static and run only once per class
+    public static void setupOnce(){
+        System.out.println("Before all");
+    }
+
+    @BeforeEach // always run before each test
+    public void setup(){
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("After Each");
+    }
 
     @Test
     public void testSumSuccess(){
