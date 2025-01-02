a26bfd882a3716b0ab1335c4a16582208788fe9a
raflynagachi <rafly.rigannagachi@gmail.com>
2025-01-02 08:45:25 +0700

    feat: login API

---
+++ spring/learn-spring-api/src/main/java/com/raflynagachi/learn_spring_api/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.raflynagachi.learn_spring_api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.raflynagachi.learn_spring_api.entity.User;
+import com.raflynagachi.learn_spring_api.model.LoginUserRequest;
+import com.raflynagachi.learn_spring_api.model.TokenResponse;
+import com.raflynagachi.learn_spring_api.model.WebResponse;
+import com.raflynagachi.learn_spring_api.service.AuthService;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(
+            path = "/api/auth/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
+        TokenResponse tokenResponse = authService.login(request);
+        return WebResponse.<TokenResponse>builder().data(tokenResponse).build();
+    }
+
+    @DeleteMapping(
+            path = "/api/auth/logout",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> logout(User user) {
+        authService.logout(user);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+}
---
+++ spring/learn-spring-api/src/main/java/com/raflynagachi/learn_spring_api/model/LoginUserRequest.java
@@ -0,0 +1,20 @@
+package com.raflynagachi.learn_spring_api.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginUserRequest {
+    
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
